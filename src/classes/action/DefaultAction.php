854fb92eeb7c0455122c39585da07b8390d2638c
Matthieu Cabot <matthieu.cabot3@etu.univ-lorraine.fr>
2025-11-02 15:23:24 +0100

    Ajout de la playlist courante sur la page principale lorsqu'elle existe

--- src/classes/action/DefaultAction.php
+++ src/classes/action/DefaultAction.php
@@ -2,9 +2,25 @@
 
 namespace iutnc\deefy\action;
 
+use iutnc\deefy\audio\lists\Playlist;
+use iutnc\deefy\repository\DeefyRepository;
+use iutnc\deefy\render\AudioListRenderer;
+
 class DefaultAction extends Action {
     public function __invoke() : string {
-            return "<h1>Bienvenu sur Deefy!</h1>";
+        $html = "<h1>Bienvenu sur Deefy!</h1>";
+        if (isset($_SESSION['user'])) {
+            if (isset($_SESSION['id_courant'])){
+
+                $html .= "<h2>Votre playlist courante</h2>";
+
+                $nom_pl = DeefyRepository::getInstance()->getNomPlaylist($_SESSION['id_courant']);
+                $playlist = DeefyRepository::getInstance()->getTrackPlaylist($_SESSION['id_courant'], $nom_pl);
+                $renderer = new AudioListRenderer($playlist);
+                $html .= $renderer->render(2);
+            }
+            
+        }
+        return $html;
     }
-    
 }
